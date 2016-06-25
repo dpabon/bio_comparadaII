8934ded063f3232899d44f821210fd98c11719ba
dane <dpabon@openmailbox.org>
2016-06-24 21:08:45 -0500

    bucle de directorios

--- Goldberg/bin/R/descarga.R
+++ Goldberg/bin/R/descarga.R
@@ -1,12 +1,39 @@
-datos <-read.csv("~/MEGAsync/bio_comparadaII/Goldberg/data/Arctostaphylos.csv")
-setwd("~/MEGAsync/bio_comparadaII/Goldberg/data/Arctostaphylos/ITS_1/")
 library(ape)
+## Numeros de acceso
+datos <-read.csv("~/MEGAsync/bio_comparadaII/Goldberg/data/Arctostaphylos.csv")
+directorios <- c("ITS_1", "ITS_2", "ITS_1+2")
+
+## Descargando las secuencias
+for(dic in 1:length(directorios)){
+  setwd(paste("~/MEGAsync/bio_comparadaII/Goldberg/data/Arctostaphylos/", directorios[dic], sep = ""))
+  for(i in 1:length(datos[,dic+1])){
+    if(is.null(i)==T){
+      print("no hay secuencia disponible")
+    }else{
+      dato <- read.GenBank(datos[i,dic+1], species.names = T)
+      write.dna(dato, paste(attr(dato, "specie"), "_", i, ".fasta", sep = ""), format = "fasta")
+    }
+  }
+}
+
 
 for(i in 1:length(datos[,2])){
-  if(i==NULL){
+  if(is.null(i)==T){
     print("no hay secuencia disponible")
   }else{
-    dato <- read.GenBank(i, species.names = T)
+    dato <- read.GenBank(datos[i,2], species.names = T)
     write.dna(dato, paste(attr(dato, "specie"), "_", i, ".fasta", sep = ""), format = "fasta")
   }
 }
+
+setwd("~/MEGAsync/bio_comparadaII/Goldberg/data/Arctostaphylos/ITS_2/")
+
+for(i in 1:length(datos[,3])){
+  if(is.null(i)==T){
+    print("no hay secuencia disponible")
+  }else{
+    dato <- read.GenBank(datos[i,3], species.names = T)
+    write.dna(dato, paste(attr(dato, "specie"), "_", i, ".fasta", sep = ""), format = "fasta")
+  }
+}
+
