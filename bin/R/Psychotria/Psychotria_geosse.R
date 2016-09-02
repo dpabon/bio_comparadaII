c4b6766b43fb4e263f6ef59d39103d8b71c1a3b3
dpabon <dpabon@openmailbox.org>
2016-09-02 11:36:01 -0500

    GEOSSE ML script

---
+++ bin/R/Psychotria/Psychotria_geosse.R
@@ -0,0 +1,35 @@
+## Calculo AIC datos empiricos
+Psychotria
+library(diversitree)
+library(BioGeoBEARS)
+arbol <- read.tree("~/MEGAsync/bio_comparadaII/result/Psychotria//final/output/Psychotria.new") 
+geogfn = "~/MEGAsync/bio_comparadaII/result/Psychotria/final/Psychotria_geo.dat"
+
+datos <- getranges_from_LagrangePHYLIP(lgdata_fn=geogfn)
+plot(arbol)
+arbol$tip.label
+datos <- as.data.frame(datos@df)
+datos1 <- vector()
+for( i in 1:nrow(datos)){
+  if(datos$K[i] ==1){
+    datos1[i] <-1
+  }
+  if(datos$O[i] ==1){
+    datos1[i] <-2
+  }
+  if(datos$M[i] ==1){
+    datos1[i] <-3
+  }
+  if(datos$H[i] ==1){
+    datos1[i] <-4
+  }
+}
+names(datos1) <- rownames(datos)
+length(datos1)
+lik1 <- make.musse(arbol, states =  datos1, k = 4)
+pars <- c(runif(20, 0,1))
+likelihod <- find.mle(lik1, x.init = pars )
+str(likelihod)
+likelihod$lnLik
+
+aic_muse_psyco <- -2 * (likelihod$lnLik) + 2 * 20
